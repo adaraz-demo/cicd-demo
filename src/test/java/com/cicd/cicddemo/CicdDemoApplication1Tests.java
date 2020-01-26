0a82933ccc31d3461002c04b170a427f4bf713ec
96656 <adaraz@red.sa>
2020-01-26 18:19:07 +0300

    test success tests

---
+++ src/test/java/com/cicd/cicddemo/CicdDemoApplication1Tests.java
@@ -0,0 +1,15 @@
+package com.cicd.cicddemo;
+
+import static org.assertj.core.api.Assertions.fail;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdDemoApplication1Tests {
+
+	@Test
+	void contextLoads() {
+		fail("not implemented");
+	}
+
+}

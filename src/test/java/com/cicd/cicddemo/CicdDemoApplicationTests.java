d65aea697a53b8416352f026a2a894aabeccfa1e
96656 <adaraz@red.sa>
2020-01-26 14:01:11 +0300

    add failed unit test

--- src/test/java/com/cicd/cicddemo/CicdDemoApplicationTests.java
+++ src/test/java/com/cicd/cicddemo/CicdDemoApplicationTests.java
@@ -1,5 +1,7 @@
 package com.cicd.cicddemo;
 
+import static org.assertj.core.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class CicdDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		fail("not implemented");
 	}
 
 }
